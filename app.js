749570b61e6362f8eb03a63101786c0c9a7959eb
Alishba shekh <alishbashabbir890gmail.com>
2026-02-01 08:31:57 -0800

    ..

--- app.js
+++ app.js
@@ -17,6 +17,19 @@ app.get('/read', async (req,res)=>{
     let users = await userModel.find();
     res.render("read" , {users})
 })
+app.get('/edit/:id', async (req,res)=>{
+    let user = await userModel.findOne({_id:req.params.id});
+    res.render("edit" , {user})
+})
+app.post('/update/:id', async (req,res)=>{
+    let{name,email,image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id:req.params.id},{
+        name,
+        email,
+        image
+    }, {new:true});
+    res.redirect("/read");
+})
 app.get('/delete/:id', async (req,res)=>{
     let users = await userModel.findOneAndDelete({_id:req.params.id});
     res.redirect("/read")
